53443587f18d1cc152d90e60aeabdc643639c156
marc <marc@ubuntu>
2025-07-18 17:49:54 +0200

    update dependencies

--- run.sh
+++ run.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -x
-# set JAVA_HOME to the first JRE we find in the eclipse plugins directory
-ECLIPSE_PLUGINS=/BigOne/extdata/eclipse/plugins/
-JAVA_HOME=`find $ECLIPSE_PLUGINS -type d -name jre`
-
+export JAVA_HOME=/home/marc/.sdkman/candidates/java/current
+export PATH=${JAVA_HOME}/bin:$PATH
 cd /BigOne/extdata/git_repos/favorite_dailies/target
-if [ ! -f java ]
-then
-	ln -s $JAVA_HOME/bin/java java
-fi
-
-nohup ./java -jar `ls -1 favorite_dailies*.jar` > ../log.txt 2>&1 &
-#./java -jar `ls -1 favorite_dailies*.jar`
+nohup java -jar `ls -1 favorite_dailies*.jar` > ../log.txt 2>&1 &
